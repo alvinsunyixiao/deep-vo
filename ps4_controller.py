725ac2fe793cab8a456f28a0f03b445065bf160f
Alvin Sun <alvinsunyixiao@gmail.com>
2022-01-13 14:27:42 -0800

    rc controlled domain randomization

---
+++ ps4_controller.py
@@ -0,0 +1,79 @@
+import pygame
+import queue
+import time
+import threading
+import typing as T
+
+import numpy as np
+
+from collections import defaultdict
+from scipy import signal
+
+class PS4Controller:
+    # buttons
+    X = 0
+    O = 1
+    TRIANGLE = 2
+    SQUARE = 3
+
+    def __init__(self,
+                 joystick_id: int = 0,
+                 num_filt_taps: int = 31,
+                 sample_freq: int = 100):
+        self.joystick_id = joystick_id
+        self.num_filt_taps = num_filt_taps
+        self.sample_freq = sample_freq
+        self.terminate_evt = threading.Event()
+        self.axes = defaultdict(float)
+        self.buttons = defaultdict(bool)
+        self.button_callbacks = {}
+        self.step = 0
+
+    def __enter__(self):
+        self.terminate_evt.clear()
+        self.event_t = threading.Thread(target=self._event_loop)
+        self.event_t.start()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.terminate_evt.set()
+        self.event_t.join()
+
+    def register_button_callback(self, button_id: int, func: T.Callable[[], None]):
+        self.button_callbacks[button_id] = func
+
+    def _event_loop(self):
+        pygame.init()
+        joy = pygame.joystick.Joystick(self.joystick_id)
+        joy.init()
+
+        while not self.terminate_evt.is_set():
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.JOYAXISMOTION:
+                    self.axes[event.axis] = event.value
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    self.buttons[event.button] = True
+                    if event.button in self.button_callbacks:
+                        self.button_callbacks[event.button]()
+                elif event.type == pygame.JOYBUTTONUP:
+                    self.buttons[event.button] = False
+
+        joy.quit()
+        print("Joystick quitted gracefully")
+
+
+if __name__ == "__main__":
+    # press "X" to stop logging
+    ctrl = PS4Controller()
+    with ctrl:
+        while not ctrl.buttons[0]:
+            print(f"Axis -- 0: {ctrl.axes[0]:.3f} "
+                  f"1: {ctrl.axes[1]:.3f} "
+                  f"2: {ctrl.axes[2]:.3f} "
+                  f"3: {ctrl.axes[3]:.3f} "
+                  f"4: {ctrl.axes[4]:.4f} "
+                  f"5: {ctrl.axes[5]:.5f} ")
+            print(f"Buttons -- 0: {ctrl.buttons[0]} "
+                  f"1: {ctrl.buttons[1]} "
+                  f"2: {ctrl.buttons[2]} "
+                  f"3: {ctrl.buttons[3]} ")
---
+++ rc_drone.py
@@ -0,0 +1,81 @@
+import airsim
+import argparse
+import datetime
+import threading
+import time
+import os
+
+import numpy as np
+
+from ps4_controller import PS4Controller
+
+CLIENT_LOCK = threading.Lock()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output-dir", type=str, required=True,
+                        help="output directory to store output")
+    return parser.parse_args()
+
+def create_trajectory_filepath(base_dir):
+    filename = time.strftime("%y-%m-%d_%H-%M-%S.pkl")
+    return os.path.join(base_dir, filename)
+
+def randomize_time(client: airsim.VehicleClient):
+    now = datetime.datetime.now()
+    delta_h = np.random.uniform(-12, 12)
+    delta_time = datetime.timedelta(hours=delta_h)
+    random_time = now + delta_time
+    print(f"Randomized time: {random_time}")
+    with CLIENT_LOCK:
+        client.simSetTimeOfDay(True, random_time.strftime("%Y-%m-%d %H:%M:%S"), update_interval_secs=5)
+
+def randomize_weather(client: airsim.VehicleClient):
+    with CLIENT_LOCK:
+        client.simSetWeatherParameter(airsim.WeatherParameter.Rain,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.Roadwetness,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.Snow,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.RoadSnow,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.MapleLeaf,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.RoadLeaf,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.Dust,
+                                      min(np.random.exponential(0.1), 1))
+        client.simSetWeatherParameter(airsim.WeatherParameter.Fog,
+                                      min(np.random.exponential(0.1), 1))
+
+if __name__ == "__main__":
+    args = parse_args()
+    output_dir = args.output_dir
+    image_dir = os.path.join(output_dir, "images")
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(image_dir, exist_ok=True)
+
+    client = airsim.MultirotorClient()
+    client.confirmConnection()
+    client.enableApiControl(True)
+    client.armDisarm(True)
+    client.simEnableWeather(True)
+
+    controller = PS4Controller()
+    controller.register_button_callback(controller.O, lambda: randomize_weather(client))
+    controller.register_button_callback(controller.TRIANGLE, lambda: randomize_time(client))
+
+    with controller:
+        while not controller.buttons[controller.X]:
+            with CLIENT_LOCK:
+                kinematics = client.simGetGroundTruthKinematics()
+                client.moveByRollPitchYawrateThrottleAsync(
+                    roll=controller.axes[3] * .6,
+                    pitch=-controller.axes[4] * .6,
+                    yaw_rate=-controller.axes[0] * 2.,
+                    throttle=(-controller.axes[1] + 1) / 2,
+                    duration=0.2,
+                )
+            time.sleep(0.01)
+
