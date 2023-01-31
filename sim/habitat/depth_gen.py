2d17fdba6c79042dcaf35021f704f7fb52b4e48e
Alvin Sun <alvinsunyixiao@gmail.com>
2023-01-30 16:25:36 -0800

    use manual data

--- sim/habitat/depth_gen.py
+++ sim/habitat/depth_gen.py
@@ -2,15 +2,13 @@ import argparse
 import io
 import pickle
 
+import cv2
 import numpy as np
 import quaternion
 from PIL import Image
 from tqdm import trange
 
 import habitat_sim as hsim
-from habitat_sim.utils import common as utils
-from habitat_sim.utils import viz_utils as vut
-from sim.habitat.viz import display_sample
 
 from utils.params import ParamDict
 from utils.pose3d import Pose3D, Rot3D
@@ -19,9 +17,6 @@ def mk_simple_cfg(scene_path: str, p: ParamDict) -> hsim.Configuration:
     # simulator backend
     sim_cfg = hsim.SimulatorConfiguration()
     sim_cfg.scene_id = scene_path
-
-    # agent
-    agent_cfg = hsim.agent.AgentConfiguration()
 
     # RGB sensor
     rgb_sensor_spec = hsim.CameraSensorSpec()
@@ -41,7 +36,15 @@ def mk_simple_cfg(scene_path: str, p: ParamDict) -> hsim.Configuration:
     depth_sensor_spec.position = [0.0, p.sensor_height, 0.0]
     depth_sensor_spec.hfov = p.img_hfov
 
+    # agent
+    agent_cfg = hsim.agent.AgentConfiguration()
     agent_cfg.sensor_specifications = [rgb_sensor_spec, depth_sensor_spec]
+    agent_cfg.action_space = {
+        "forward": hsim.agent.ActionSpec("move_forward", hsim.agent.ActuationSpec(amount=0.04)),
+        "left": hsim.agent.ActionSpec("turn_left", hsim.agent.ActuationSpec(amount=2.)),
+        "right": hsim.agent.ActionSpec("turn_right", hsim.agent.ActuationSpec(amount=2.)),
+        "back": hsim.agent.ActionSpec("move_backward", hsim.agent.ActuationSpec(amount=0.04)),
+    }
 
     return hsim.Configuration(sim_cfg, [agent_cfg])
 
@@ -54,8 +57,6 @@ def parse_args() -> argparse.Namespace:
                         help="path to the scene file")
     parser.add_argument("--nav-mesh", type=str, default=None,
                         help="path to nav mesh for autonomous exploration")
-    parser.add_argument("--viz", action="store_true",
-                        help="specify to visualize generated data")
     parser.add_argument("-o", "--output", type=str, required=True,
                         help="output path to store output")
 
@@ -77,52 +78,59 @@ if __name__ == "__main__":
 
     agent = sim.initialize_agent(0)
     data = []
-    for i in trange(params.num_samples):
-        # sample random position
-        while True:
-            nav_point = sim.pathfinder.get_random_navigable_point()
-            if sim.pathfinder.distance_to_closest_obstacle(
-                    nav_point, params.obstacle_search_radius) == params.obstacle_search_radius:
-                break
-
-        # set agent pose
-        agent_state = agent.get_state()
-        agent_state.position = nav_point
-        agent_state.rotation = np.quaternion(1., 0., 0., 0.)
-        agent.set_state(agent_state)
-
-        # set camera pose
-        agent_state = agent.get_state()
-
-        # EUS coordinate !!??
-        # @see: https://aihabitat.org/docs/habitat-sim/habitat_sim.geo.html
-        pyr = np.random.uniform([-params.perturb.pitch, -np.pi, -params.perturb.roll],
-                                [params.perturb.pitch, np.pi, params.perturb.roll])
-        R_yaw = quaternion.from_rotation_vector([0., pyr[1], 0.])
-        R_pitch = quaternion.from_rotation_vector([pyr[0], 0., 0.])
-        R_roll = quaternion.from_rotation_vector([0., 0., pyr[2]])
-        R_all = R_roll * R_pitch * R_yaw
-        for sensor in agent_state.sensor_states:
-            agent_state.sensor_states[sensor].rotation = R_all
-        agent.set_state(agent_state, infer_sensor_states=False)
-
+    while True:
         obs = sim.get_sensor_observations()
-        if args.viz:
-            display_sample(obs["color_sensor"], obs["depth_sensor"])
+        cv2.imshow("color", obs["color_sensor"])
+        key = cv2.waitKey(1)
 
-        # save to data
-        color_img = Image.fromarray(obs["color_sensor"], mode="RGBA")
-        color_img_bytes = io.BytesIO()
-        color_img.save(color_img_bytes, format="PNG")
+        if key == ord('w'):
+            sim.step("forward")
+        elif key == ord('a'):
+            sim.step("left")
+        elif key == ord('d'):
+            sim.step("right")
+        elif key == ord('s'):
+            sim.step("back")
+        elif key == ord('q'):
+            print(f"Collected {len(data)} samples")
+            break
+        elif key == ord('r'):
+            # set camera pose
+            agent_state = agent.get_state()
 
-        world_R_sensor = Rot3D(np.array([R_all.x, R_all.y, R_all.z, R_all.w]))
-        world_T_sensor = Pose3D(world_R_sensor, agent_state.sensor_states["depth_sensor"].position)
+            # EUS coordinate !!??
+            # @see: https://aihabitat.org/docs/habitat-sim/habitat_sim.geo.html
+            pyr = np.random.uniform(-params.perturb.R * np.ones(3), params.perturb.R * np.ones(3))
+            R_yaw = quaternion.from_rotation_vector([0., pyr[1], 0.])
+            R_pitch = quaternion.from_rotation_vector([pyr[0], 0., 0.])
+            R_roll = quaternion.from_rotation_vector([0., 0., pyr[2]])
+            R_all = R_roll * R_pitch * R_yaw
+            vert = np.random.uniform(-params.perturb.t, params.perturb.t)
+            for sensor in agent_state.sensor_states:
+                agent_state.sensor_states[sensor].rotation = agent_state.rotation * R_all
+                agent_state.sensor_states[sensor].position += np.array([0., vert, 0.])
+            agent.set_state(agent_state, infer_sensor_states=False)
+            obs = sim.get_sensor_observations()
+            cv2.imshow("sampled", obs["color_sensor"])
 
-        data.append({
-            "image_png": color_img_bytes.getvalue(),
-            "depth": obs["depth_sensor"],
-            "pose": world_T_sensor.to_storage().numpy(),
-        })
+            # save to data
+            color_img = Image.fromarray(obs["color_sensor"], mode="RGBA")
+            color_img_bytes = io.BytesIO()
+            color_img.save(color_img_bytes, format="PNG")
+
+            R = agent_state.sensor_states["depth_sensor"].rotation
+            t = agent_state.sensor_states["depth_sensor"].position
+            world_R_sensor = Rot3D(np.array([R.x, R.y, R.z, R.w]))
+            world_T_sensor = Pose3D(world_R_sensor, t)
+
+            data.append({
+                "image_png": color_img_bytes.getvalue(),
+                "depth": obs["depth_sensor"],
+                "pose": world_T_sensor.to_storage().numpy(),
+            })
+
+            # revert sensor position
+            agent.set_state(agent_state)
 
     with open(args.output, "wb") as f:
         pickle.dump(data, f)
--- sim/habitat/params.py
+++ sim/habitat/params.py
@@ -4,13 +4,12 @@ from utils.params import ParamDict
 
 PARAMS = ParamDict(
     img_size_hw = (144, 256),
-    img_hfov = 120.,
+    img_hfov = 90.,
     random_seed = 4242,
-    num_samples = 200,
     obstacle_search_radius = 1.0,
     sensor_height = 1.5,
     perturb = ParamDict(
-        roll = math.radians(30.),
-        pitch = math.radians(30.),
+        R = math.radians(20.),
+        t = 0.4,
     ),
 )
